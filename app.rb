584526d210157d44dd825527330a7c6c9470ca81
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-06-06 11:07:36 +0300

    commit tree 78e9ed02532c91d58b6e87ba1786036ffb63b1e5

---
+++ app.rb
@@ -0,0 +1,20 @@
+#app.rb
+puts "Hello World!"
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+puts
+print "Pass this test, please."
+puts [1, 2, 3]
+puts [1, 2, 3].to_s
+puts [1, 2, 3].inspect
+p [1, 2, 3]
+p [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+puts 'hey'
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+puts 'hey'
+puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }].to_s
+puts 'hey'
+puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }].inspect
+puts 'hey'
+puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }].pretty_inspect
